cc8b31f0dc6f22eb931eb7053d002c7aae894e26
Bharat <hibharatj@gmail.com>
2025-07-01 16:47:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-//your JS code here. If required.
+const button = document.querySelector('input[type="button"]');
+const colorSelect = document.getElementById('colorSelect');
+
+// button.onclick = function () {
+// 	const selectedIndex = colorSelect.selectedIndex;
+	
+// 	if (selectedIndex !== -1) {
+// 		colorSelect.remove(selectedIndex);
+		
+// 	}
+// };
+
+button.addEventListener('click', () => {
+	const selectedIndex = colorSelect.selectedIndex;
+
+	if(selectedIndex !== -1) {
+		colorSelect.remove(selectedIndex);
+	}
+})
+
+// function startFunc() {
+// 	const selectedIndex = colorSelect.selectedIndex;
+
+// 	if(selectedIndex !== -1) {
+// 		colorSelect.remove(selectedIndex);
+// 	}
+// }
